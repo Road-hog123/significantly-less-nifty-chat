09bcb8949dd7e71071655cb793c7ccf351ffffd3
Nathan Burnham <nathan@nathanburnham.uk>
2024-11-07 12:18:06 +0000

    Bump version number

--- chat-monitor.user.js
+++ chat-monitor.user.js
@@ -3,7 +3,7 @@
 // @namespace      https://roadhog123.co.uk/
 // @description    inlines Images, GIPHY GIFs & YouTube Thumbnails in Twitch chat
 // @match          https://www.twitch.tv/*
-// @version        1.1
+// @version        1.2
 // @updateURL      https://raw.githubusercontent.com/road-hog123/significantly-less-nifty-chat/master/chat-monitor.user.js
 // @downloadURL    https://raw.githubusercontent.com/road-hog123/significantly-less-nifty-chat/master/chat-monitor.user.js
 // @require        https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
